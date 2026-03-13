60608e1825b963111e949ef333c312020d05597d
Gissel Quitian <gisselvanessaq@yahoo.es>
2026-03-12 23:27:33 -0500

    cambio en download_model

--- download_model.py
+++ download_model.py
@@ -1,5 +1,7 @@
 import os
 import gdown  #type: ignore
+from dotenv import load_dotenv #type: ignore
+load_dotenv()
 
 MODEL_PATH = os.getenv("CHECKPOINT_PATH", "models/best_model.pt")
 MODEL_URL  = os.getenv("MODEL_URL") 
@@ -7,7 +9,7 @@ MODEL_URL  = os.getenv("MODEL_URL")
 os.makedirs("models", exist_ok=True)
 
 if not os.path.exists(MODEL_PATH):
-    print("Descargando modelo desde Drive...")
+    print("Descargando modelo desde Drive")
     gdown.download(MODEL_URL, MODEL_PATH, quiet=False, fuzzy=True)
     print("Modelo listo.")
 else:
