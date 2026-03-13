eb2164fa0faca593761545b24441a26877fe5232
Gissel Quitian <gisselvanessaq@yahoo.es>
2026-03-12 23:00:45 -0500

    acceso al modelo.pt en drive

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python download_model.py && uvicorn app.main:app --host 0.0.0.0 --port $PORT
---
+++ download_model.py
@@ -0,0 +1,14 @@
+import os
+import gdown  #type: ignore
+
+MODEL_PATH = os.getenv("CHECKPOINT_PATH", "models/best_model.pt")
+MODEL_URL  = os.getenv("MODEL_URL") 
+
+os.makedirs("models", exist_ok=True)
+
+if not os.path.exists(MODEL_PATH):
+    print("Descargando modelo desde Drive...")
+    gdown.download(MODEL_URL, MODEL_PATH, quiet=False, fuzzy=True)
+    print("Modelo listo.")
+else:
+    print("Modelo ya existe, saltando descarga.")
